e4389de41b2184ad665a6bcc503b7abf93c7d334
kelsonthony <k.thony@gmail.com>
2023-03-31 10:22:26 -0300

    update part-1

---
+++ customer/src/main/java/com/kelsonthony/domain/service/CustomerService.java
@@ -0,0 +1,73 @@
+package com.kelsonthony.domain.service;
+
+import com.kelsonthony.api.dto.CustomerDTO;
+import com.kelsonthony.domain.model.CustomerEntity;
+import com.kelsonthony.domain.repository.CustomerRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class CustomerService {
+
+    @Inject
+    private CustomerRepository customerRepository;
+
+    public List<CustomerDTO> findAllCustomers() {
+        List<CustomerDTO> customers = new ArrayList<>();
+
+        customerRepository.findAll().stream().forEach(item -> {
+            customers.add(mapCustomerEntityToDTO(item));
+        });
+
+        return customers;
+    }
+
+    public void createNewCustomer(CustomerDTO customerDTO){
+        customerRepository.persist(mapCustomerDtoToEntity(customerDTO));
+    }
+
+    private CustomerDTO mapCustomerEntityToDTO(CustomerEntity customer) {
+        CustomerDTO customerDTO = new CustomerDTO();
+
+        customerDTO.setAddress(customer.getAddress());
+        customerDTO.setAge(customer.getAge());
+        customerDTO.setEmail(customer.getEmail());
+        customerDTO.setName(customer.getName());
+        customerDTO.setPhone(customer.getPhone());
+
+        return customerDTO;
+    }
+
+    public void changeCustomer(Long id, CustomerDTO customerDTO) {
+
+        CustomerEntity customer = customerRepository.findById(id);
+
+        customer.setName(customerDTO.getName());
+        customer.setAddress(customerDTO.getAddress());
+        customer.setPhone(customerDTO.getPhone());
+        customer.setEmail(customer.getEmail());
+        customer.setAge(customerDTO.getAge());
+
+        customerRepository.persist(customer);
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    private CustomerEntity mapCustomerDtoToEntity(CustomerDTO customer) {
+        CustomerEntity customerEntity = new CustomerEntity();
+
+        customerEntity.setAddress(customer.getAddress());
+        customerEntity.setAge(customer.getAge());
+        customerEntity.setEmail(customer.getEmail());
+        customerEntity.setName(customer.getName());
+        customerEntity.setPhone(customer.getPhone());
+
+        return customerEntity;
+    }
+
+}
---
+++ customer/src/test/java/com/kelsonthony/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.kelsonthony;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
